2e60f64657a99e3f888c1a4246acccd7f3897fec
rkamisetti792 <kamisettirakesh@gmail.com>
2019-03-13 16:55:23 +0530

    first commit

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Welcome RKamisetti. aman and anant is kankakachora This is wonderful! This is my first Jenkins PHP200 page using OpenShift Online</p>'; ?> 
+ <?php echo '<p>Welcome RKamisetti... aman and anant is kankakachora This is wonderful! This is my first Jenkins PHP200 page using OpenShift Online</p>'; ?> 
  </body>
 </html>
